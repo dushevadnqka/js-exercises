98f09f96bfab40770eb2e802282e873bda2db10c
Georgi Alexandrov <georgi.alexandrov@reify.technology>
2020-11-17 20:38:35 +0200

    add lesson 2.1

--- src/lessons/Lesson2.ts
+++ src/lessons/Lesson2.ts
@@ -2,8 +2,26 @@ import { ILesson } from "./ILesson";
 
 class Lesson2 implements ILesson {
 
+    rotateArray (arrayOfNumbers: Array<number>, k: number): Array<number> {
+        let i;
+        for (i = 0; i < k; i++) { 
+            const popEd = arrayOfNumbers.pop()
+            if (popEd) {
+                arrayOfNumbers.unshift(popEd)
+            }
+        }
+
+        return arrayOfNumbers
+    }
+    
+    // array rotation
     getResult (input: string|number): string {
-        return `test lesson 2 input(${input})`
+        const arrayOfNumbers = [3, 8, 9, 7, 6] //[1, 2, 3, 4]
+        const inputCatsed = typeof input === "number" ? input : parseInt(input, 10)
+
+        const result = this.rotateArray(arrayOfNumbers, inputCatsed)
+
+        return `[${result.join(", ")}]`
     }
 }
 
